c653e109df727a1a72d08980d39c3d5e04b37ba0
Sc-202010122235\Administrator <azskw1101@gmail.com>
2022-01-06 00:19:09 +0900

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_responsive_app/firebase_config.dart';
 import 'package:instagram_responsive_app/responsive/mobile_screen_layout.dart';
 import 'package:instagram_responsive_app/responsive/responsive_layout_screen.dart';
 import 'package:instagram_responsive_app/responsive/web_screen_layout.dart';
 import 'package:instagram_responsive_app/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        // apiKey: 'AIzaSyBy3ydTw-NdRGJ4EZu1oD9YjtgrjGoHokI',
+        apiKey: FirebaseConfig.API_KEY,
+        appId: FirebaseConfig.APP_ID,
+        messagingSenderId: FirebaseConfig.MESSAGING_SENDER_ID,
+        projectId: FirebaseConfig.PROJECT_ID,
+        storageBucket: FirebaseConfig.STORAGE_BUCKET,
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
