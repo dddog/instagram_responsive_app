1e35d44b9c8c860302acc2bd23a36a897731f140
Sc-202010122235\Administrator <azskw1101@gmail.com>
2022-01-10 22:00:14 +0900

    search screen add

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_responsive_app/screens/add_post_screen.dart';
 import 'package:instagram_responsive_app/screens/feed_screen.dart';
+import 'package:instagram_responsive_app/screens/search_screen.dart';
 
 const webScreenSize = 600;
 
 const homeScreenItems = [
   FeedScreen(),
-  Text('search'),
+  SearchScreen(),
   AddPostScreen(),
   Text('fav'),
   Text('profile'),
